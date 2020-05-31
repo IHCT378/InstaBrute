758583b91e139c5ab59a8483e519fe2f0a1f0d46
IHCTchannel <66212597+IHCT378@users.noreply.github.com>
2020-05-31 23:43:57 +0700

    InstaIHCT.py

--- insta.sh
+++ insta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# FB by: https://www.youtube.com/c/HA-MRX
-# Instagram: @iemprator_ha_mrx
+# FB by: https://www.youtube.com/c/IHCTchannel
+# INDONESIA HACKER CYBER TEAM
 
 trap 'store;exit 1' 2
 string4=$(openssl rand -hex 32 | cut -c 1-4)
@@ -43,9 +43,9 @@ fi
 banner() {
 
 printf "\e[1;92m     _                                      \e[0m\n"
-printf "\e[1;92m _  | | https://www.youtube.com/c/HA-MRX    \e[0m\n"
+printf "\e[1;92m _  | | INDONESIA HACKER CYBER TEAM         \e[0m\n"
 printf "\e[1;92m( \ | | ____    ___  _| |_  _____           \e[0m\n"
-printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ |  Ha3MrX  \e[0m\n"
+printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ |  IHCT.   \e[0m\n"
 printf "\e[1;77m(_/ | || | | ||___ |  | |_ / ___ |  _____   \e[0m\n"
 printf "\e[1;77m    |_||_| |_|(___/    \__)\_____| (_____)  \e[0m\n"
 printf "\n"
